9eeb5e8dc1ae2d31c76bcb00dffaf16b40da1a3f
sosogfm <sunnzoca@gmail.com>
2026-09-01 19:00:48 -0300

    feat(evolution): Configurar Jest para projetos TypeScript + Vite + React.

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
+
+export default config;
